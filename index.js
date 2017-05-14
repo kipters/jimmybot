7f1eb8c1baa45104f6177975ddf2ba9f905d0912
Fabio Di Peri <fabiodiperi@outlook.com>
2017-05-14 21:24:28 +0200

    :sparkles: Added shitting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -9,8 +10,37 @@ app.use(bodyParser.json());
 
 const key = process.env.botkey;
 
+const triggers = [
+  "js",
+  "javascript",
+  "docker",
+  "systemd"
+];
+
 app.post(`/bot/${key}`, function(request, response) {
-  console.log(JSON.stringify(request.body));
+  const update = request.body;
+  
+  if (body.message === undefined || body.message.text === undefined) {
+    response.sendStatus(200);
+    return;
+  }
+
+  const text = body.message.text;
+  const chatId = body.chat_id;
+  const msgId = body.message_id;
+
+  const triggered = triggers.filter(item => {
+    return text.includes(item);
+  }).forEach(item => {
+    const msg = {
+      chat_id: chatId,
+      text: `${item} merda 💩`,
+      reply_to_message_id: msgId
+    };
+    
+    request.post(`https://api.telegram.org/bot${botkey}/sendMessage`, { json: msg });
+  });
+
   response.sendStatus(200);
 });
 
